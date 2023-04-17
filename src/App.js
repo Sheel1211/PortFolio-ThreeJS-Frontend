31ddf4d91dec28c33373c88a0ce7ded091fc9aaf
Sheel1211 <96932878+Sheel1211@users.noreply.github.com>
2023-04-17 12:38:46 +0530

    added path and loader

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import AdminPanel from "./components/Admin/AdminPanel";
 import Timeline from "./components/Admin/Timeline";
 import YouTube from "./components/Admin/YouTube";
 import {Projects as Project} from "./components/Admin/Projects";
+import Loader from "./components/Loader/Loader";
 
 function App() {
   const { isAuthenticated } = useSelector((state) => state.login);
@@ -28,7 +29,7 @@ function App() {
   return (
     <Router>
       {loading ? (
-        <div>Loading</div>
+        <Loader />
       ) : (
         <>
           <Header />
